4486e6bbb1ce21850a314866b9c054f91679abc4
Stewart <stilondanga@gmail.com>
2022-02-01 14:04:15 +0300

    add javascript/scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -3,13 +3,13 @@ const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 const timestamps = [];
 
 timestamps.unshift(getTimestamp());
-/*
-function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/*
 function getRandomKey() {
   return keys[getRandomNumber(0, keys.length-1)];
 }
